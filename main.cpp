67ebae1d88f6f6993e8cc39941a63e47e2235e03
Danya Shlyahov <ukraniandollar@gmail.com>
2021-10-11 18:32:53 +0300

    Done philosophers

--- main.cpp
+++ main.cpp
@@ -17,37 +17,53 @@ public:
 	Fork() : binary_semaphore(1) {}
 };
 
-
 void SignalEating(int pos) {
 	cout_semaphore.acquire();
 	cout << "Philosopher " << pos << " is eating\n";
 	cout_semaphore.release();
 }
 
-void Eat(unsigned int phil_position, unsigned int eat_time) {
+void SignalFinishEating(int pos) {
+	cout_semaphore.acquire();
+	cout << "Philosopher " << pos << " finished eating\n";
+	cout_semaphore.release();
+}
+
+void Eat(unsigned int phil_position, unsigned int eat_time, Fork* left, Fork* right) {
 	//cout << eat_time << endl;
-
-
-	SignalEating(phil_position);
-	this_thread::sleep_for(chrono::seconds(eat_time));
-
+	while (true) {
+		if (left->try_acquire()) {
+			if (right->try_acquire()) {
+				SignalEating(phil_position);
+				this_thread::sleep_for(chrono::seconds(eat_time));
+				SignalFinishEating(phil_position);
+				break;
+			}
+			else {
+				left->release();
+			}
+		}
+	}
+	left->release();
+	right->release();
 }
 
 int main(){
 	srand(time(0));
 
-	unsigned int phil_count = 3;
+	unsigned int phil_count = 5;
 
 	vector<Fork> forks(phil_count);
 	vector<Philosopher> philosophers(phil_count);
 
 	for (unsigned int i = 0; i < phil_count; i++) {
-		philosophers[i] = Philosopher(Eat, i, 1 + rand() % 4);
+		philosophers[i] = Philosopher(Eat, i, 1 + rand() % 4, &forks[i % phil_count], &forks[(i + 1) % phil_count]);
 	}
 
 	for (unsigned int i = 0; i < phil_count; i++) {
 		philosophers[i].join();
 	}
 
-	getchar();
+	cout << "\nDone\n";
+
 }
